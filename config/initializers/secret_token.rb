9da0b7c7b727720adf0ed09949658547a7722293
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-10-04 11:41:06 +0300

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RepetitionApp::Application.config.secret_key_base = '9bd7c200eb02422cc46c141632e63f569df3550329fe10b7a9a78037780eb7e05066a10e238653c1d6eef5410500fb2336b1c293e25fb054acba95cd0ad73097'

6fba5be7fea6e1449379160e9a3fc69747b80480
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-10-13 20:00:33 +0300

    Complete task 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,6 +13,27 @@ describe "User pages" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
+
+			describe "after submission" do
+				before { click_button submit }
+
+				it { should have_title("Sign up") }
+				it { should have_content('error') }
+				it { should have_selector("li") }
+			end
+
+			describe "invalid password confirmation" do
+				before do
+					fill_in "Name",         with: "Example User"
+					fill_in "Email",        with: "user@example.com"
+					fill_in "Password",     with: "foobar"
+					fill_in "Confirmation", with: "mistake"
+					click_button submit
+				end
+
+				it { should have_content("error") }
+				it { should have_selector("li", text: "confirmation") }
+			end
 		end
 
 		describe "with valid information" do
@@ -26,6 +47,14 @@ describe "User pages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
+
+			describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
 		end
 	end
 
@@ -35,5 +64,9 @@ describe "User pages" do
 
 		it { should have_content(user.name) }
 		it { should have_title(user.name) }
+
+		describe "gravatar" do
+			it { should have_selector("img") }
+		end
 	end
 end

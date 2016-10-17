82fbffd1b8965209eeeb14edd66922b15efd3f98
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-10-17 22:37:58 +0300

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -49,12 +49,13 @@ describe "User pages" do
 			end
 
 			describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
+				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com') }
 
-        it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
+				it { should have_link('Sign out') }
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			end
 		end
 	end
 

e52ff9a866314566eec3d2391a9a67c169ab7fa3
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-10-17 19:34:03 +0300

    Create test for failed athentication

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "Authentication" do
+	subject { page }
+
+	describe "signin page" do
+		before { visit signin_path }
+
+		it { should have_content('Sign in') }
+		it { should have_title('Sign in') }
+
+		describe "sign in" do
+			before { visit signin_path }
+
+			describe "with invslid information" do
+				before { click_button "Sign in" }
+
+				it { should have_title('Sign in') }
+				it { should have_selector('div.alert.alert-error') }
+			end
+		end
+	end
+end
